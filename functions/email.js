3f7cf11accd4db7242a3ce9dc3e419bc16d2283f
Lucas Vocos <lucasvocos@gmail.com>
2020-04-27 16:12:58 -0400

    use event handler for emailer

--- functions/email.js
+++ functions/email.js
@@ -1,48 +1,79 @@
-"use strict";
-require("dotenv").config();
+// "use strict";
+// require("dotenv").config();
 
-const express = require("express");
-const serverless = require("serverless-http");
-const cors = require("cors");
-const bodyParser = express.json();
+// const express = require("express");
+// const serverless = require("serverless-http");
+// const cors = require("cors");
+// const bodyParser = express.json();
+// const sgMail = require("@sendgrid/mail");
+// const app = express();
+
+// const router = express.Router();
+
+// sgMail.setApiKey(process.env.REACT_APP_SENDGRID_KEY);
+
+// app.use(cors());
+// app.use(bodyParser);
+// app.use("/.netlify/functions/email", router);
+
+// router.route("/").post(bodyParser, (req, res) => {
+//   const { name, email, subject, message } = req.body;
+
+//   const msg = {
+//     to: "lucasvocos@gmail.com",
+//     from: "web@fireflydroneshows.com",
+//     subject: "Message from SendGrid",
+//     text: `Name: ${name}. Email: ${email}. Subject: ${subject}. Message: ${message}.`,
+//     html: `<h1>New inquiry:</h1><ul><li>Name: ${name}.</li><li>Email: ${email}.</li><li> Subject: ${subject}. </li><li>Message: ${message}.</li></ul>`,
+//   };
+
+//   sgMail
+//     .send(msg)
+//     .then((response) => {
+//       console.log(`Message from ${email} sent!`, response);
+//     })
+//     .catch((error) => {
+//       console.log(error.response.body);
+//     });
+
+//   res.status(200).json({
+//     name,
+//     email,
+//     subject,
+//     message,
+//   });
+// });
+
+// module.exports = app;
+// module.exports.handler = serverless(app);
+
 const sgMail = require("@sendgrid/mail");
-const app = express();
+const { REACT_APP_SENDGRID_KEY } = process.env;
 
-const router = express.Router();
+exports.handler = async (event, context, callback) => {
+  const payload = JSON.parse(event.body);
+  const { name, email, subject, message } = payload;
 
-sgMail.setApiKey(process.env.REACT_APP_SENDGRID_KEY);
-
-app.use(cors());
-app.use(bodyParser);
-app.use("/.netlify/functions/email", router);
-
-router.route("/").post(bodyParser, (req, res) => {
-  const { name, email, subject, message } = req.body;
+  sgMail.setApiKey(REACT_APP_SENDGRID_KEY);
 
   const msg = {
     to: "lucasvocos@gmail.com",
     from: "web@fireflydroneshows.com",
-    subject: "Message from SendGrid",
-    text: `Name: ${name}. Email: ${email}. Subject: ${subject}. Message: ${message}.`,
+    subject: `Contact Form Submission from ${email}`,
     html: `<h1>New inquiry:</h1><ul><li>Name: ${name}.</li><li>Email: ${email}.</li><li> Subject: ${subject}. </li><li>Message: ${message}.</li></ul>`,
   };
 
-  sgMail
-    .send(msg)
-    .then((response) => {
-      console.log(`Message from ${email} sent!`, response);
-    })
-    .catch((error) => {
-      console.log(error.response.body);
-    });
+  try {
+    await sgMail.send(msg);
 
-  res.status(200).json({
-    name,
-    email,
-    subject,
-    message,
-  });
-});
-
-module.exports = app;
-module.exports.handler = serverless(app);
+    return {
+      statusCode: 200,
+      body: "Message sent",
+    };
+  } catch (e) {
+    return {
+      statusCode: e.code,
+      body: e.response.body,
+    };
+  }
+};
