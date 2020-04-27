31d94f35420aaa8cb3f22910367eea168b675ad0
Lucas Vocos <lucasvocos@gmail.com>
2020-04-27 16:17:20 -0400

    clean up comments

--- functions/email.js
+++ functions/email.js
@@ -1,54 +1,9 @@
-// "use strict";
-// require("dotenv").config();
-
-// const express = require("express");
-// const serverless = require("serverless-http");
-// const cors = require("cors");
-// const bodyParser = express.json();
-// const sgMail = require("@sendgrid/mail");
-// const app = express();
-
-// const router = express.Router();
-
-// sgMail.setApiKey(process.env.REACT_APP_SENDGRID_KEY);
-
-// app.use(cors());
-// app.use(bodyParser);
-// app.use("/.netlify/functions/email", router);
-
-// router.route("/").post(bodyParser, (req, res) => {
-//   const { name, email, subject, message } = req.body;
-
-//   const msg = {
-//     to: "lucasvocos@gmail.com",
-//     from: "web@fireflydroneshows.com",
-//     subject: "Message from SendGrid",
-//     text: `Name: ${name}. Email: ${email}. Subject: ${subject}. Message: ${message}.`,
-//     html: `<h1>New inquiry:</h1><ul><li>Name: ${name}.</li><li>Email: ${email}.</li><li> Subject: ${subject}. </li><li>Message: ${message}.</li></ul>`,
-//   };
-
-//   sgMail
-//     .send(msg)
-//     .then((response) => {
-//       console.log(`Message from ${email} sent!`, response);
-//     })
-//     .catch((error) => {
-//       console.log(error.response.body);
-//     });
-
-//   res.status(200).json({
-//     name,
-//     email,
-//     subject,
-//     message,
-//   });
-// });
-
-// module.exports = app;
-// module.exports.handler = serverless(app);
-
 const sgMail = require("@sendgrid/mail");
-const { REACT_APP_SENDGRID_KEY } = process.env;
+const {
+  REACT_APP_SENDGRID_KEY,
+  REACT_APP_SENDGRID_TO,
+  REACT_APP_SENDGRID_FROM,
+} = process.env;
 
 exports.handler = async (event, context, callback) => {
   const payload = JSON.parse(event.body);
@@ -57,8 +12,8 @@ exports.handler = async (event, context, callback) => {
   sgMail.setApiKey(REACT_APP_SENDGRID_KEY);
 
   const msg = {
-    to: "lucasvocos@gmail.com",
-    from: "web@fireflydroneshows.com",
+    to: REACT_APP_SENDGRID_TO,
+    from: REACT_APP_SENDGRID_FROM,
     subject: `Contact Form Submission from ${email}`,
     html: `<h1>New inquiry:</h1><ul><li>Name: ${name}.</li><li>Email: ${email}.</li><li> Subject: ${subject}. </li><li>Message: ${message}.</li></ul>`,
   };
