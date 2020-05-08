882ec687d65dbccf4172755844e0229b0147abef
Lucas Vocos <lucasvocos@gmail.com>
2020-05-08 12:06:17 -0400

    cleanup comments

--- functions/email.js
+++ functions/email.js
@@ -15,13 +15,6 @@ exports.handler = async (event, context, callback) => {
     to: REACT_APP_SENDGRID_TO,
     from: REACT_APP_SENDGRID_FROM,
     replyTo: email || REACT_APP_SENDGRID_FROM,
-    // templateId: "d-f9ef2cb4852a4b17b4f4b3ce5a02ac59",
-    // dynamic_template_data: {
-    //   name,
-    //   email,
-    //   subject,
-    //   message,
-    // },
     subject: `New contact submission from ${email}`,
     html: `<main style="background-color: #D5DEE2; height: 100vh; font-family: 'Helvetica'; color: black;"><div style="position: sticky; left: 20px; top: 20px; margin-bottom: 100px;"><img src="http://cdn.mcauto-images-production.sendgrid.net/6f931ad6136eca3c/c1da312b-92bb-4661-ad0d-18b665902207/499x200.png" style="height: 40px;" /></div><section class='message' style="background-color: white; padding: 20px; max-width: 800px; width: 80%; margin: 0 auto; min-height: 50vh; border-radius: 10px;"><h2 style="font-weight: normal">New Form Submission:</h2><section class='submission'><ul><li>Name: ${name}</li<li>Email: ${email}</li><li>Subject: ${subject}</li><li>Message: ${message}</li></ul><h3 style="font-weight: normal; margin-top: 40px">Reply to this email directly to message <span style="text-decoration: underline">${email}</span></h3></section></section></main>`,
   };
