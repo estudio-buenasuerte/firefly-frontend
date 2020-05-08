74389d9c0ec84b2943feed5615401256efabcfe3
Lucas Vocos <lucasvocos@gmail.com>
2020-05-08 12:12:49 -0400

    update mailer

--- src/util/mailer.js
+++ src/util/mailer.js
@@ -2,26 +2,22 @@ const MailerService = {
   async sendMail(data) {
     const { name, email, subject, message } = data;
 
-    return fetch(
-      "https://firefly-drone-shows.netlify.app/.netlify/functions/email",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name,
-          email,
-          subject,
-          message,
-        }),
-      }
-    )
+    fetch("https://firefly-drone-shows.netlify.app/.netlify/functions/email", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        subject,
+        message,
+      }),
+    })
       .then((res) => {
         if (res.ok) {
           return res.json();
         } else {
-          debugger;
           return res.json().then((e) => {
             Promise.reject(e);
           });
