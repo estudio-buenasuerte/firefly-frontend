75dda76eb696215a9fa63f5d9d1ad5051956fd54
Lucas Vocos <lucasvocos@gmail.com>
2020-05-20 12:31:53 -0400

    update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,64 +1,50 @@
-require("dotenv").config();
+require('dotenv').config();
 module.exports = {
-  siteMetadata: {
-    title: `Firefly Drone Shows`,
-    description: `Firefly provides an immersive experience that takes our client's events and brands to new heights. One of the only FAA licensed companies in the world, Firefly offers the latest technology operated by the most experienced pilots.`,
-    author: `Buena Suerte Studio <info@buena-suerte.studio>`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Firefly Drone Shows`,
-        short_name: `Firefly Drone Shows`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon/drone.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: "gatsby-source-sanity",
-      options: {
-        projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-        dataset: process.env.REACT_APP_SANITY_DATASET,
-        token: process.env.REACT_APP_SANITY_API_TOKEN,
-      },
-    },
-    `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-112748982-1",
-
-        head: false,
-
-        anonymize: true,
-
-        respectDNT: true,
-
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-
-        pageTransitionDelay: 0,
-
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "fireflydroneshows.com",
-      },
-    },
-  ],
+	siteMetadata: {
+		title: `Firefly Drone Shows`,
+		description: `Firefly provides an immersive experience that takes our client's events and brands to new heights. One of the only FAA licensed companies in the world, Firefly offers the latest technology operated by the most experienced pilots.`,
+		author: `Buena Suerte Studio <info@buena-suerte.studio>`,
+	},
+	plugins: [
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				trackingId: 'UA-112748982-1',
+				head: true,
+				anonymize: true,
+			},
+		},
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`,
+			},
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		`gatsby-plugin-styled-components`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `Firefly Drone Shows`,
+				short_name: `Firefly Drone Shows`,
+				start_url: `/`,
+				background_color: `#663399`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/images/favicon/drone.png`, // This path is relative to the root of the site.
+			},
+		},
+		{
+			resolve: 'gatsby-source-sanity',
+			options: {
+				projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+				dataset: process.env.REACT_APP_SANITY_DATASET,
+				token: process.env.REACT_APP_SANITY_API_TOKEN,
+			},
+		},
+		`gatsby-plugin-sass`,
+	],
 };
