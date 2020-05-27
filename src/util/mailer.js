c2e131035642d100379c87fda765bf18d70a4ccc
Lucas Vocos <lucasvocos@gmail.com>
2020-05-27 09:52:30 -0400

    relative fetch

--- src/util/mailer.js
+++ src/util/mailer.js
@@ -1,30 +1,30 @@
 const MailerService = {
-  async sendMail(data) {
-    const { name, email, subject, message } = data;
+	async sendMail(data) {
+		const { name, email, subject, message } = data;
 
-    fetch("https://firefly-drone-shows.netlify.app/.netlify/functions/email", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name,
-        email,
-        subject,
-        message,
-      }),
-    })
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        } else {
-          return res.json().then((e) => {
-            Promise.reject(e);
-          });
-        }
-      })
-      .catch((error) => console.error(error));
-  },
+		fetch('/.netlify/functions/email', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				name,
+				email,
+				subject,
+				message,
+			}),
+		})
+			.then((res) => {
+				if (res.ok) {
+					return res.json();
+				} else {
+					return res.json().then((e) => {
+						Promise.reject(e);
+					});
+				}
+			})
+			.catch((error) => console.error(error));
+	},
 };
 
 export default MailerService;
