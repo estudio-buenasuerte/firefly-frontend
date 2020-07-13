3c3ac9775aae6de132130e603784904a3dd457cd
Lucas Vocos <lucasvocos@gmail.com>
2020-07-13 16:06:08 -0400

    use SanityMuxVideo for work page

--- src/components/WorkAsset/WorkAsset.js
+++ src/components/WorkAsset/WorkAsset.js
@@ -1,162 +1,153 @@
-import React from "react";
-import {
-  Player,
-  ControlBar,
-  CurrentTimeDisplay,
-  VolumeMenuButton,
-} from "video-react";
-import styled from "styled-components";
-import "video-react/dist/video-react.css";
+import React from 'react';
+import styled from 'styled-components';
+import 'video-react/dist/video-react.css';
+import SanityMuxPlayer from 'sanity-mux-player';
 
 const AssetWrapper = styled.article`
-  width: 100%;
-  padding: 0 0 20px;
-  font-family: "Zwizz";
+	width: 100%;
+	padding: 0 0 20px;
+	font-family: 'Zwizz';
 
-  img {
-    padding-bottom: 20px;
-  }
+	img {
+		padding-bottom: 20px;
+	}
 
-  img,
-  video {
-    max-width: 100%;
-    width: 100%;
-    &:focus {
-      outline: 1px solid transparent;
-    }
-  }
+	img,
+	video {
+		max-width: 100%;
+		width: 100%;
+		&:focus {
+			outline: 1px solid transparent;
+		}
+	}
 
-  .video-react {
-    position: relative;
-    margin-bottom: 20px;
-    font-family: "Zwizz" !important;
-    &:focus {
-      outline: 1px solid transparent;
-    }
+	.video-react {
+		position: relative;
+		margin-bottom: 20px;
+		font-family: 'Zwizz' !important;
+		&:focus {
+			outline: 1px solid transparent;
+		}
 
-    .video-react-big-play-button {
-      display: none;
-    }
+		.video-react-big-play-button {
+			display: none;
+		}
 
-    .video-react-control-bar {
-      position: absolute;
-      bottom: 5px;
-      display: flex;
-      background-color: transparent;
-      padding: 0 10px;
-      align-items: center;
+		.video-react-control-bar {
+			position: absolute;
+			bottom: 5px;
+			display: flex;
+			background-color: transparent;
+			padding: 0 10px;
+			align-items: center;
 
-      .video-react-icon-fullscreen {
-        margin-left: auto;
-      }
-      .video-react-play-control {
-        background-color: transparent;
-        color: white;
-        border: 0;
-        padding: 0;
-        font-family: "Zwizz" !important;
+			.video-react-icon-fullscreen {
+				margin-left: auto;
+			}
+			.video-react-play-control {
+				background-color: transparent;
+				color: white;
+				border: 0;
+				padding: 0;
+				font-family: 'Zwizz' !important;
 
-        &:before {
-          position: initial;
-          text-shadow: none;
-        }
-        &:hover {
-          &:before {
-            text-shadow: none;
-          }
-        }
+				&:before {
+					position: initial;
+					text-shadow: none;
+				}
+				&:hover {
+					&:before {
+						text-shadow: none;
+					}
+				}
 
-        &.video-react-paused {
-          &:before {
-            font-size: 14px;
-            content: "Play";
-            @media (min-width: 1024px) {
-              font-size: 18px;
-            }
-          }
-        }
-        &.video-react-playing {
-          &:before {
-            font-size: 14px;
-            content: "Pause";
-            @media (min-width: 1024px) {
-              font-size: 18px;
-            }
-          }
-        }
-      }
-    }
-  }
+				&.video-react-paused {
+					&:before {
+						font-size: 14px;
+						content: 'Play';
+						@media (min-width: 1024px) {
+							font-size: 18px;
+						}
+					}
+				}
+				&.video-react-playing {
+					&:before {
+						font-size: 14px;
+						content: 'Pause';
+						@media (min-width: 1024px) {
+							font-size: 18px;
+						}
+					}
+				}
+			}
+		}
+	}
 
-  @media (min-width: 1024px) {
-    box-sizing: border-box;
-    padding: 0 10px 40px;
-    &.one-quarter {
-      width: 25%;
-    }
-    &.two-quarter {
-      width: 50%;
-    }
-    &.three-quarter {
-      width: 75%;
-    }
-    &.four-quarter {
-      width: 100%;
-    }
+	@media (min-width: 1024px) {
+		box-sizing: border-box;
+		padding: 0 10px 40px;
+		&.one-quarter {
+			width: 25%;
+		}
+		&.two-quarter {
+			width: 50%;
+		}
+		&.three-quarter {
+			width: 75%;
+		}
+		&.four-quarter {
+			width: 100%;
+		}
 
-    .video-react-control-bar {
-      bottom: 10px;
-      font-size: 18px;
-      padding: 0 20px;
-    }
-  }
+		.video-react-control-bar {
+			bottom: 10px;
+			font-size: 18px;
+			padding: 0 20px;
+		}
+	}
 `;
 
 const Title = styled.p`
-  margin: 0 0 10px;
-  color: white;
-  font-size: 18px;
+	margin: 0 0 10px;
+	color: white;
+	font-size: 18px;
 `;
 
 const SubTitle = styled.p`
-  margin: 0;
-  color: #d5dee2;
-  font-size: 14px;
-  @media (min-width: 1024px) {
-    font-size: 18px;
-    padding-right: 20px;
-    box-sizing: border-box;
-  }
+	margin: 0;
+	color: #d5dee2;
+	font-size: 14px;
+	@media (min-width: 1024px) {
+		font-size: 18px;
+		padding-right: 20px;
+		box-sizing: border-box;
+	}
 `;
 
-const WorkAsset = ({ asset, autoPlay }) => {
-  const isAssetImage = asset.type === "image/jpeg";
+const WorkAsset = ({ asset }) => {
+	const isVideo = asset.type === 'video';
 
-  return (
-    <AssetWrapper className={asset.layout}>
-      {asset && (
-        <React.Fragment>
-          {isAssetImage ? (
-            <img src={asset.url} alt={asset.title ? asset.title : ""} />
-          ) : (
-            <Player
-              playsInline={true}
-              autoPlay={true}
-              loop={true}
-              muted={false}
-              src={asset.url}
-            >
-              <ControlBar autoHide={true}>
-                <CurrentTimeDisplay order={4.1} />
-                <VolumeMenuButton disabled />
-              </ControlBar>
-            </Player>
-          )}
-        </React.Fragment>
-      )}
-      {asset.title && <Title>{asset.title}</Title>}
-      {asset.description && <SubTitle>{asset.description}</SubTitle>}
-    </AssetWrapper>
-  );
+	return (
+		<AssetWrapper className={asset.layout}>
+			{asset && (
+				<React.Fragment>
+					{isVideo ? (
+						<SanityMuxPlayer
+							assetDocument={asset.video}
+							autoload={true}
+							autoplay={true}
+							showControls={true}
+							muted={false}
+							loop={true}
+						/>
+					) : (
+						<img src={asset.imageUrl} alt={asset.title ? asset.title : ''} />
+					)}
+				</React.Fragment>
+			)}
+			{asset.title && <Title>{asset.title}</Title>}
+			{asset.description && <SubTitle>{asset.description}</SubTitle>}
+		</AssetWrapper>
+	);
 };
 export default WorkAsset;
--- src/pages/work.js
+++ src/pages/work.js
@@ -1,66 +1,97 @@
-import React, { useState } from "react";
-import { useStaticQuery, graphql } from "gatsby";
-import Layout from "../components/Layout/Layout";
-import styled from "styled-components";
-import SEO from "../components/seo";
-import WorkAsset from "../components/WorkAsset/WorkAsset";
+import React, { useState } from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Layout from '../components/Layout/Layout';
+import styled from 'styled-components';
+import SEO from '../components/seo';
+import WorkAsset from '../components/WorkAsset/WorkAsset';
 
 const WorkList = styled.main`
-  display: flex;
-  flex-direction: column;
-  padding: 125px 20px;
+	display: flex;
+	flex-direction: column;
+	padding: 125px 20px;
 
-  @media (min-width: 1024px) {
-    padding: 150px 40px;
-    flex-direction: row;
-    flex-wrap: wrap;
-  }
+	@media (min-width: 1024px) {
+		padding: 150px 40px;
+		flex-direction: row;
+		flex-wrap: wrap;
+	}
 `;
 
 const Work = () => {
-  const data = useStaticQuery(graphql`
-    {
-      allSanityWork {
-        nodes {
-          workList {
-            asset {
-              asset {
-                url
-                mimeType
-              }
-            }
-            title
-            description
-            assetLayout
-          }
-        }
-      }
-    }
-  `);
-  const [workList] = useState(data.allSanityWork.nodes[0].workList);
+	const data = useStaticQuery(graphql`
+		{
+			allSanityWork {
+				nodes {
+					workList {
+						_key
+						asset {
+							asset {
+								url
+								mimeType
+							}
+						}
+						title
+						description
+						assetLayout
+						image {
+							asset {
+								url
+								fluid {
+									base64
+									aspectRatio
+									src
+									srcSet
+									srcWebp
+									srcSetWebp
+									sizes
+								}
+							}
+						}
+						workMuxVideo {
+							_key
+						}
+					}
+					_rawWorkList(resolveReferences: { maxDepth: 100 })
+				}
+			}
+		}
+	`);
 
-  return (
-    <Layout>
-      <SEO title="Work" description="" />
+	const { _rawWorkList, workList } = data.allSanityWork.nodes[0];
 
-      {workList && (
-        <WorkList>
-          {workList.map((item) => (
-            <WorkAsset
-              key={item.asset.asset.url}
-              asset={{
-                type: item.asset.asset.mimeType,
-                url: item.asset.asset.url,
-                title: item.title,
-                description: item.description,
-                layout: item.assetLayout,
-              }}
-            />
-          ))}
-        </WorkList>
-      )}
-    </Layout>
-  );
+	let newList = workList.map((item) => {
+		return item.workMuxVideo?.hasOwnProperty('_key')
+			? _rawWorkList.find((raw) => raw._key === item._key)
+			: item;
+	});
+
+	const [projectList] = useState(newList);
+
+	return (
+		<Layout>
+			<SEO title="Work" description="" />
+
+			{projectList && (
+				<WorkList>
+					{projectList.map((item) => {
+						return (
+							<WorkAsset
+								key={item._key}
+								asset={{
+									type: item?.type,
+									imageUrl: item.asset.asset.url,
+									title: item.title,
+									description: item.description,
+									layout: item.assetLayout,
+									video: item?.workMuxVideo?.asset,
+								}}
+							/>
+						);
+					})}
+				</WorkList>
+			)}
+		</Layout>
+	);
 };
 
 export default Work;
