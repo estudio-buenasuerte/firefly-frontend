8e0e34036d1a8c0bacf7738b133df5edc0a30275
Lucas Vocos <lucasvocos@gmail.com>
2020-09-09 09:12:25 -0400

    gatsby browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,6 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+// Site Credit
+console.log(`
+Site Credit
+===========
+Design/Development : https://buena-suerte.studio
+`);
